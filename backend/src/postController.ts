78d9cd57337f6ce0b7f90012b86cd6fdbbd064a8
B-Mugnol <bruno_mugnol@hotmail.com>
2023-12-16 18:02:26 -0300

    Add basic functional return to getFullPost

--- backend/src/postController.ts
+++ backend/src/postController.ts
@@ -1,5 +1,24 @@
 import { Request, Response } from "express"
+import axios from 'axios'
 
 export const getFullPost = async (req: Request, res: Response) => {
-	res.sendStatus(200)
+	try {
+		const { data: postData } = await axios.get(
+			`https://jsonplaceholder.typicode.com/posts/${req.params?.postId}`
+		)
+
+		const { data: commentsData } = await axios.get(
+			`https://jsonplaceholder.typicode.com/posts/${req.params?.postId}/comments`
+		)
+
+		const fullPostData = {
+			post: postData,
+			comments: commentsData
+		}
+
+		res.send(fullPostData)
+
+	} catch (error) {
+		res.sendStatus(400)
+	}
 }
