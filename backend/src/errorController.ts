430f8e3bff4e40f6071dc6b0d9cce787ecd59598
B-Mugnol <bruno_mugnol@hotmail.com>
2023-12-16 18:01:27 -0300

    Add descriptive comments

--- backend/src/errorController.ts
+++ backend/src/errorController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express"
 
+// Default error
 const pageNotFound = async (req: Request, res: Response) => {
 	res.sendStatus(404)
 }
--- backend/src/router.ts
+++ backend/src/router.ts
@@ -2,8 +2,10 @@ import express from 'express'
 import error from './errorController'
 import { getFullPost } from './postController'
 
+// Post Routes
 export const postRouter = express.Router()
 postRouter.get("/:postId", getFullPost)
 
+// Error Routes
 export const errorRouter = express.Router()
 errorRouter.all("*", error)
