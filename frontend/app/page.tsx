0851bc584a6789fc8900088d15cd289637a7802a
B-Mugnol <bruno_mugnol@hotmail.com>
2023-12-17 16:37:40 -0300

    Add buttons to load other posts

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,36 +1,58 @@
+'use client'
+
+import { useState } from 'react';
 import Image from 'next/image'
 import styles from './page.module.css'
 import { Post } from './ui/post'
 
 export default function Home() {
-	return (
-		<main className={styles.main}>
-			<div className={styles.description}>
-				<p>
-					A very very simple (and ugly){' '}
-					<code className={styles.code}>Next.js made website</code>
-				</p>
-				<div>
-					<a
-						href="https://github.com/B-Mugnol"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						By{' '}
-						<Image
-							src="/zombie.svg"
-							alt="Zombie Logo"
-							  className={styles.zombieLogo}
-							width={100}
-							height={100}
-							priority
-						/>
-					</a>
-				</div>
-			</div>
+	const [currentPostId, setCurrentPostId] = useState(1);
 
-			<Post postId={1} />
-			<Post postId={2} />
-		</main>
+	const nextPost = () => {
+		setCurrentPostId(currentPostId + 1)
+	}
+
+	const previousPost = () => {
+		if (currentPostId <= 1) return
+		setCurrentPostId(currentPostId - 1)
+	}
+
+	return (
+		<div className={styles.core}>
+			{
+				currentPostId == 1 ?
+					<></>:
+					<button  className={styles.changePostButton} onClick={previousPost}>Previous post</button>
+			}
+			<main className={styles.main}>
+				<div className={styles.description}>
+					<p>
+						A very very simple (and ugly){' '}
+						<code className={styles.code}>Next.js made website</code>
+					</p>
+					<div>
+						<a
+							href="https://github.com/B-Mugnol"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							By{' '}
+							<Image
+								src="/zombie.svg"
+								alt="Zombie Logo"
+								className={styles.zombieLogo}
+								width={100}
+								height={100}
+								priority
+							/>
+						</a>
+					</div>
+				</div>
+
+				<Post postId={currentPostId} />
+
+			</main>
+			<button className={styles.changePostButton} onClick={nextPost}>Next post</button>
+		</div>
 	)
 }
