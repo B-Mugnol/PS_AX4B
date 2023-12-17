eb987dbad66e1b0e08ff40abc80b5c67a4cdc360
B-Mugnol <bruno_mugnol@hotmail.com>
2023-12-17 16:37:07 -0300

    Use hooks instead of async component

--- frontend/app/ui/post.tsx
+++ frontend/app/ui/post.tsx
@@ -1,30 +1,38 @@
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 import styles from './post.module.css'
 import { PostData, PostComponentProperty, CommentData } from '../lib/definitions'
 import { Comment } from './comment'
 
-export async function Post({ postId }: PostComponentProperty) {
-	try {
-		const { data } = await axios.get(`${process.env.URL || "http://localhost:3003"}/post/${postId}`)
-		const post: PostData = data.post
-		const comments: CommentData[] = data.comments
+export function Post({ postId }: PostComponentProperty) {
+	const [post, setPostData] = useState<PostData | null>(null)
+	const [comments, setComments] = useState<CommentData[] | null>(null)
+	const [isLoading, setLoading] = useState(true)
 
-		return (
-			<div className={styles.post}>
-				<p className={styles.title}>{post?.title}</p>
-				<p className={styles.content}>{post?.body}</p>
-				<div className={styles.comments}>
-					{comments.map(
-						({ id, name, body }) => <Comment key={id} name={name} content={body} />
-					)}
-				</div>
+	useEffect(() => {
+		console.log("currentPostId: " + postId)
+		setLoading(true)
+		axios.get(`${process.env.URL || "http://localhost:3003"}/post/${postId}`)
+			.then(({ data }) => {
+				setPostData(data.post)
+				setComments(data.comments)
+				setLoading(false)
+			})
+	}, [postId])
+
+
+	if (isLoading) return <p>Loading, please wait...</p>
+	if (!post) return <p>We ran into some issues. Please wait a few moments before reloading.</p>
+
+	return (
+		<div className={styles.post}>
+			<p className={styles.title}>{post?.title}</p>
+			<p className={styles.content}>{post?.body}</p>
+			<div className={styles.comments}>
+				{comments?.map(
+					({ id, name, body }) => <Comment key={id} name={name} content={body} />
+				)}
 			</div>
-		)
-	} catch (error) {
-		return (
-			<div>
-				{postId == 1 ? "We ran into some issues. Please wait a few moments before reloading." : ""}
-			</div>
-		)
-	}
+		</div>
+	)
 }
